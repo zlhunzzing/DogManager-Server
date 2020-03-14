72403cb6a061ead919415ef6758ee271db1df4b4
김태선 <kirca322@gmail.com>
2020-03-14 17:26:44 +0900

    3가지 유닛테스트 추가
    - 새로운 이벤트를 생성하는 API 테스트 (POST /api/admin/events/entry)
    - 생성되어있는 모든 이벤트 리스트를 보내주는  API 테스트 (GET /api/admin/events/list)
    - 특정 이벤트 하나의 정보를 보내주는 API 테스트 (GET /api/admin/events/entry/:id)

--- src/test/server.spec.ts
+++ src/test/server.spec.ts
@@ -6,12 +6,60 @@ chai.use(chaiHttp);
 const expect = chai.expect;
 import { createTypeormConnection } from "../utils/createTypeormConnection";
 
+const dataForCreateEvent = (num: number = 1) => {
+  return {
+    event_title: `new event ${num}`,
+    start_date: "2020-04-01",
+    end_date: "2020-04-30",
+    detail_page_url: "detail page url",
+    button_url: "button url",
+    button_image: "button image",
+    banner_image: "banner image",
+    page_image: "page image"
+  };
+};
+
 describe("Implemented testcase", () => {
   before(async () => {
     await createTypeormConnection();
   });
 
-  it("should get all event list", done => {
+  it("should create a new event", done => {
+    const agent = chai.request.agent(app);
+    agent
+      .post("/api/admin/events/entry")
+      .send(dataForCreateEvent())
+      .end((err, res) => {
+        if (err) done(err);
+        expect(res).to.have.status(201);
+        done();
+      });
+  });
+
+  it("should get all event lists", done => {
+    const agent = chai.request.agent(app);
+    agent
+      .post("/api/admin/events/entry")
+      .send(dataForCreateEvent(1))
+      .send(dataForCreateEvent(2))
+      .then(() => {
+        agent.get("/api/admin/events/list").end((err, res) => {
+          if (err) done(err);
+          expect(res).to.have.status(200);
+          expect(res.body.eventList.length).to.equal(2);
+          expect(res.body.eventList[0]).has.all.keys([
+            "id",
+            "event_title",
+            "start_date",
+            "end_date",
+            "detail_page_url"
+          ]);
+          done();
+        });
+      });
+  });
+
+  it("should get information of selected event", done => {
     const agent = chai.request.agent(app);
     // const insertData = await agent.post("/api/admin/events/entry").send({
     //   event_title: "postman2",
@@ -44,20 +92,12 @@ describe("Implemented testcase", () => {
     // });
     agent
       .post("/api/admin/events/entry")
-      .send({
-        event_title: "new event",
-        start_date: "2020-04-01",
-        end_date: "2020-04-30",
-        detail_page_url: "detail page url",
-        button_url: "button url",
-        button_image: "button image",
-        banner_image: "banner image",
-        page_image: "page image"
-      })
-      .then(res => {
-        agent.get("/api/admin/events/entry/1").end((err, res2) => {
-          expect(res2).to.have.status(200);
-          expect(res2.body).has.all.keys([
+      .send(dataForCreateEvent())
+      .then(() => {
+        agent.get("/api/admin/events/entry/1").end((err, res) => {
+          if (err) done(err);
+          expect(res).to.have.status(200);
+          expect(res.body).has.all.keys([
             "event_title",
             "start_date",
             "end_date",
