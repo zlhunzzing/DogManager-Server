4daac725dffe5a6d2cef66835ccbf09699849bf7
김태선 <kirca322@gmail.com>
2020-03-16 11:54:56 +0900

    service 레이어 리펙토링
    - 객체로 지정되어 있던 service파일 => class로 선언하는 방식으로 수정
    - 각 메소드들의 parameter 타입과 return 타입 지정

--- src/controller/admin/index.ts
+++ src/controller/admin/index.ts
@@ -1,29 +1,37 @@
 import { Request, Response } from "express";
 import adminService from "../../service/admin";
 
+const service = new adminService();
+
 export default {
-  addEventController: async (req: Request, res: Response) => {
-    await adminService.addEventService(req.body);
+  addEventController: async (req: Request, res: Response): Promise<void> => {
+    await service.addEventService(req.body);
     res.status(201).end();
   },
 
-  putEventController: async (req: Request, res: Response) => {
-    await adminService.putEventService(req.body, req.params.id);
+  putEventController: async (req: Request, res: Response): Promise<void> => {
+    await service.putEventService(req.body, req.params.id);
     res.status(200).end();
   },
 
-  getEventListController: async (req: Request, res: Response) => {
-    const result = await adminService.getEventListService();
+  getEventListController: async (
+    req: Request,
+    res: Response
+  ): Promise<void> => {
+    const result = await service.getEventListService();
     res.status(200).json({ eventList: result });
   },
 
-  getEventEntryController: async (req: Request, res: Response) => {
-    const result = await adminService.getEventEntryService(req.params.id);
+  getEventEntryController: async (
+    req: Request,
+    res: Response
+  ): Promise<void> => {
+    const result = await service.getEventEntryService(req.params.id);
     res.status(200).json(result);
   },
 
-  deleteEventController: async (req: Request, res: Response) => {
-    await adminService.deleteEventService(req.params.id);
+  deleteEventController: async (req: Request, res: Response): Promise<void> => {
+    await service.deleteEventService(req.params.id);
     res.status(200).end();
   }
 };
