7711d6ba5722d861345d3411074719aba33b43a1
김태선 <kirca322@gmail.com>
2020-03-16 16:30:38 +0900

    s3버킷에 이미지 업로드하는 기능 연습

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,29 @@
-import * as express from "express";
+import express from "express";
 import { Request, Response } from "express";
-import * as bodyParser from "body-parser";
+import bodyParser from "body-parser";
 import { createConnection } from "typeorm";
 import adminRouter from "./routes/admin";
 import userRouter from "./routes/user";
-import * as cors from "cors";
+import cors from "cors";
 import { createTypeormConnection } from "./utils/createTypeormConnection";
+import AWS from "aws-sdk";
+import path from "path";
+import multer from "multer";
+import multerS3 from "multer-s3";
+AWS.config.loadFromPath(__dirname + "/../awsconfig.json");
+
+const s3 = new AWS.S3();
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: "dogmate-image",
+    key: (req, file, cb) => {
+      const extension = path.extname(file.originalname);
+      cb(null, Date.now().toString() + extension);
+    },
+    acl: "public-read-write"
+  })
+});
 
 const app = express();
 const PORT: number = 3000;
@@ -17,6 +35,11 @@ if (process.env.NODE_ENV !== "test") {
 }
 app.use("/api/user", userRouter);
 app.use("/api/admin", adminRouter);
+app.post("/upload", upload.single("imgFile"), (req, res, next) => {
+  const imgFile = req.file;
+  console.log(imgFile);
+  res.json(imgFile);
+});
 
 // start express server
 app.listen(PORT, async () => {
