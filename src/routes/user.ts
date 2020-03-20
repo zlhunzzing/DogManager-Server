97b2b33b7c2821d75d6ec51b31f53d82bb6e4851
zlhunzzing <zlhunzzing@gmail.com>
2020-03-20 11:01:53 +0900

    admin signin api 작성
    - 관리자 로그인
    - jwt 토큰

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -7,12 +7,12 @@ router.get("/events/list", userController.getEventListController);
 
 router.get("/events/entry/:url", userController.getEventEntryController);
 
-router.post("/signup", userController.signupController);
+// router.post("/signup", userController.signupController);
 
-router.post("/signin", userController.signinController);
+// router.post("/signin", userController.signinController);
 
-router.post("/coupon", userController.addCouponController);
+// router.post("/coupon", userController.addCouponController);
 
-router.get("/coupon/list", userController.getCouponListController);
+// router.get("/coupon/list", userController.getCouponListController);
 
 export default router;
