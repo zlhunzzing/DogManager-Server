26bd64d9cc08eea5e6a683b8049192306c1f54a3
zlhunzzing <zlhunzzing@gmail.com>
2020-03-23 11:56:26 +0900

    controller api class 문법으로 수정

--- src/controller/user/index.ts
+++ src/controller/user/index.ts
@@ -8,33 +8,27 @@ import UserService from "../../services/user";
 const service = new UserService();
 import jwt from "jsonwebtoken";
 
-export default {
-  getEventListController: async (
-    req: Request,
-    res: Response
-  ): Promise<void> => {
+export default class UserController {
+  async getEventListController(req: Request, res: Response): Promise<void> {
     const result = await service.getEventListService();
     res.status(200).json({ eventList: result });
-  },
+  }
 
-  getEventEntryController: async (
-    req: Request,
-    res: Response
-  ): Promise<void> => {
+  async getEventEntryController(req: Request, res: Response): Promise<void> {
     const result = await service.getEventEntryService(req.params.url);
     res.status(200).json(result);
-  },
+  }
 
-  signupController: async (req: Request, res: Response): Promise<void> => {
+  async signupController(req: Request, res: Response): Promise<void> {
     const result = await service.signupService(req.body);
     if (result["key"] === "already exist") {
       res.status(409).send("email already exist");
     } else {
       res.status(201).end();
     }
-  },
+  }
 
-  signinController: async (req: Request, res: Response): Promise<void> => {
+  async signinController(req: Request, res: Response): Promise<void> {
     const result = await service.signinService(req.body);
     if (result["key"] === "unvalid user") {
       res.status(409).send("unvalid user");
@@ -42,4 +36,4 @@ export default {
       res.status(200).json({ token: result["key"] });
     }
   }
-};
+}
