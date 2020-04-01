b1fa76d4def5573069d9bec9cdacd94f5a911c34
kirca322 <kirca322@gmail.com>
2020-04-01 16:13:56 +0900

    socket에 바뀐 jwt secret key 적용

--- src/socket.ts
+++ src/socket.ts
@@ -6,11 +6,15 @@ import { RoomModels, ChatModels } from "./models";
 const roomModels = new RoomModels();
 const chatModels = new ChatModels();
 
-const verifyToken = (token, secretKey) => {
+const verifyToken = (token, secretKeyList) => {
   return new Promise((resolve, reject) => {
-    jwt.verify(token, secretKey, (err, decoded) => {
-      if (err) reject(err);
-      else resolve(decoded);
+    jwt.verify(token, secretKeyList[0], (err, decoded) => {
+      if (err) {
+        jwt.verify(token, secretKeyList[1], (err2, decoded2) => {
+          if (err2) reject(err);
+          else resolve(decoded2);
+        });
+      } else resolve(decoded);
     });
   });
 };
@@ -43,9 +47,12 @@ export default function socketInfo(nsp) {
     socket.on("login", async data => {
       let tokenInfo;
       try {
-        tokenInfo = await verifyToken(data.token, process.env.JWT_SECRET_KEY);
+        tokenInfo = await verifyToken(data.token, [
+          process.env.JWT_ADMIN_SECRET_KEY,
+          process.env.JWT_USER_SECRET_KEY
+        ]);
       } catch (err) {
-        socket.emit("error", err);
+        nsp.emit("error", err);
         console.log(err);
       }
       if (tokenInfo.isUser) {
@@ -77,9 +84,12 @@ export default function socketInfo(nsp) {
     socket.on("chat", async data => {
       let tokenInfo;
       try {
-        tokenInfo = await verifyToken(data.token, process.env.JWT_SECRET_KEY);
+        tokenInfo = await verifyToken(data.token, [
+          process.env.JWT_ADMIN_SECRET_KEY,
+          process.env.JWT_USER_SECRET_KEY
+        ]);
       } catch (err) {
-        socket.emit("error", err);
+        nsp.emit("error", err);
         console.log(err);
       }
       if (tokenInfo.isUser) {
